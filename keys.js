7b3d59dda9d6ff69b07b8232d6014a103f469803
Ally Smith <allysmith@Allys-MacBook-Air.local>
2018-02-13 16:52:02 -0600

    keys info

--- keys.js
+++ keys.js
@@ -11,3 +11,9 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+module.exports = {
+	spotify: exports.spotify,
+	twitter: exports.twitter
+}
+
--- liri.js
+++ liri.js
@@ -1,4 +1,9 @@
 require("dotenv").config();
+require("node-spotify-api");
+require("twitter");
+
+var keys = ("./keys.js")
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
